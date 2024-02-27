6da2e7c9dbaa002493da3903debe0b5d1f8c9b3d
nualimah12 <alimahn123@gmail.com>
2024-02-27 19:13:09 +0700

    commit

---
+++ Pertemuan 1/tabel/resources/views/welcome.blade.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 80%;
+  
+}
+
+td, th {
+  border: 1px solid darkblue;
+  text-align: left;
+  padding: 8px;
+  background-color: #96D4D4;
+}
+
+tr:nth-child(even) {
+  background-color: blue;
+}
+
+th{
+    text-align: center;
+}
+</style>
+</head>
+<body>
+
+<h2>DAFTAR DOSEN PENGAJAR</h2>
+
+<table>
+  <tr>
+    <th>Nama Dosen</th>
+    <th>Mata Kuliah</th>
+    <th>Ruang Dosen</th>
+  </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Dasar Pemrogaman Web</td>
+    <td>Ruang Dosen 1</td>
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Basis Data Lanjut</td>
+    <td>Ruang Dosen 1</td>
+  </tr>
+  <tr>
+    <td>Ernst Handel</td>
+    <td>Struktur Data</td>
+    <td>Ruang Dosen 2</td>
+  </tr>
+  <tr>
+    <td>Island Trading</td>
+    <td>Manajemen Proyek</td>
+    <td>Ruang Dosen 2</td>
+  </tr>
+  <tr>s
+    <td>Laughing Bacchus Winecellars</td>
+    <td>Kecerdasan Bisnis</td>
+    <td>Ruang Dosen 1</td>
+  </tr>
+  <tr>
+    <td>Magazzini Alimentari Riuniti</td>
+    <td>Pemrogaman Web</td>
+    <td>Ruang Dosen 2</td>
+  </tr>
+</table>
+
+</body>
+</html>
+
