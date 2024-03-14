d63858de08842f6a10723d04bd426358ca8cbea0
nualimah12 <alimahn123@gmail.com>
2024-03-14 08:29:33 +0700

    praktikum 2.6

--- Pertemuan 3/PWL_POS/app/Http/Controllers/UserController.php
+++ Pertemuan 3/PWL_POS/app/Http/Controllers/UserController.php
@@ -9,20 +9,22 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index(){
+    $user = UserModel::all();
+    return view('user', ['data' => $user]);
     //$user = UserModel::find(1);
     //$user = UserModel::where('level_id',1)->first();
     //$user = UserModel::firstWhere('level_id',1);
     //$user = UserModel::findOrFail(1);
    // $user = UserModel::where('level_id', 2)->count();
    // dd($user); 
-   $user = UserModel::create(
+   /*$user = UserModel::create(
     [
             'username' => 'manager11',
             'nama'=> 'Manager11',
             'password'=> Hash::make('12345'),
             'level_id'=> 2,
             /*'username' => 'manager',
-            'nama'=> 'Manager',*/
+            'nama'=> 'Manager',
     ],
     );
     $user->username = 'manager12';
@@ -33,7 +35,47 @@ class UserController extends Controller
     $user->wasChanged('username');
     $user->wasChanged(['username','level_id']);
     $user->wasChanged('nama');
-    dd($user->wasChanged(['nama','username']));
+    dd($user->wasChanged(['nama','username']));*/
    // return view('user', ['data' => $user]);
+    }
+    public function tambah(){
+    return view ('user_tambah');
+    }
+
+    public function tambah_simpan(Request $request){
+        UserModel::create([
+            'username'=> $request->username,
+            'nama'=> $request->nama,
+            'password' => Hash::make('$request->password'),
+            'level_id' => $request->level_id
+        ]);
+        return view ('user_tambah');
+    }
+
+    public function ubah($id)
+    {
+        $user = UserModel::find($id);
+        return view('user_ubah',['data' => $user]);
+    }
+
+    public function ubah_simpan($id, Request $request){
+        $user = UserModel::find($id);
+
+        $user->username = $request->username;
+        $user->nama = $request->nama;
+        $user->password = Hash::make('$request->password');
+        $user->level_id = $request->level_id;
+        
+        $user->save();
+
+        return redirect('/user');
+    }
+
+    public function hapus($id)
+    {
+        $user = UserModel::find($id);
+        $user -> delete();
+
+        return redirect('/user');
     }
 }
