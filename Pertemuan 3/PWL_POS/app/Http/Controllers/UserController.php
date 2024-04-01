974439c7f7568fcdd8bf3cb570c66c53d86891dc
nualimah12 <alimahn123@gmail.com>
2024-04-01 22:05:35 +0700

    js 7 praktikum 4

--- Pertemuan 3/PWL_POS/app/Http/Controllers/UserController.php
+++ Pertemuan 3/PWL_POS/app/Http/Controllers/UserController.php
@@ -22,13 +22,18 @@ class UserController extends Controller
         ];
 
         $activeMenu = 'user';
-        return view('user.index',['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu]);
+        $level = LevelModel::all();
+
+        return view('user.index',['breadcrumb' => $breadcrumb, 'page' => $page, 'level'=> $level, 'activeMenu' => $activeMenu]);
     }
     // Ambil data user dalam bentuk json untuk datatables 
     public function list(Request $request) 
     { 
         $users = UserModel::select('user_id', 'username', 'nama', 'level_id') ->with('level'); 
     
+        if($request->level_id){
+            $users->where('level_id',$request->level_id);
+        }
         return DataTables::of($users) 
         ->addIndexColumn() // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex) 
             ->addColumn('aksi', function ($user) {  // menambahkan kolom aksi 
