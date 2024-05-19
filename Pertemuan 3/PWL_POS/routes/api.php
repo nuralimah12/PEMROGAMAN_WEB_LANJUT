98ad40d5550ac7eb8ed296524de386a3ac099b3a
nualimah12 <alimahn123@gmail.com>
2024-05-19 17:25:49 +0700

    js 10 prak 4

--- Pertemuan 3/PWL_POS/routes/api.php
+++ Pertemuan 3/PWL_POS/routes/api.php
@@ -4,6 +4,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\LevelController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -26,6 +28,14 @@ Route::post('logout', \App\Http\Controllers\Api\LogoutController::class)->name('
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+
+Route::get('levels', [LevelController::class, 'index']);
+Route::post('levels', [LevelController::class, 'store']);
+Route::get('levels/{level}', [LevelController::class, 'show']);
+Route::put('levels/{level}', [LevelController::class, 'update']);
+Route::delete('levels/{level}', [LevelController::class, 'destroy']);
+
+
 
 
 
